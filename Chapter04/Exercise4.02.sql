eb4684730ccfc09c59eb6a73412e3c3b52685e78
ManasaVk243 <manasak@packtpub.com>
2019-12-30 10:15:24 +0530

    Create Exercise4.02.sql

---
+++ Chapter04/Exercise4.02.sql
@@ -0,0 +1,4 @@
+SELECT ProductCategoryName AS CATEGORY, ProductCategoryID AS ID 
+FROM ProductCategories;
+SELECT ProductCategoryName AS 'PRODUCT CATEGORY', ProductCategoryID AS ID 
+FROM ProductCategories;
