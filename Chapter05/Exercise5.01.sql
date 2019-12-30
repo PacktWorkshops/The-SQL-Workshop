33c6c4d56cf1d29e9cf549f83b00ef618acd0562
ManasaVk243 <manasak@packtpub.com>
2019-12-30 10:19:13 +0530

    Create Exercise5.01.sql

---
+++ Chapter05/Exercise5.01.sql
@@ -0,0 +1,8 @@
+use PACKT_ONLINE_SHOP;
+SELECT ProductName AS 'High-value Products', NetRetailPrice
+FROM Products
+WHERE NetRetailPrice > 14.99
+USE PACKT_ONLINE_SHOP;
+SELECT  ProductName AS 'High-value Products', NetRetailPrice
+FROM Products
+WHERE NetRetailPrice >= 14.99
