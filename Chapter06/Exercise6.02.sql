7f72cbd233c957b24ba32a08a73df75ce9e9b481
ManasaVk243 <manasak@packtpub.com>
2019-12-30 10:46:15 +0530

    Update Exercise6.02.sql

--- Chapter06/Exercise6.02.sql
+++ Chapter06/Exercise6.02.sql
@@ -5,3 +5,13 @@ Orders.OrderNumber,
 Orders.OrderStatus
 
 FROM Orders RIGHT JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+
+SELECT Customers.FirstName,
+Customers.LastName,
+Customers.Email , 
+Orders.OrderNumber,
+Orders.OrderStatus
+
+FROM Orders RIGHT JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+WHERE Orders.OrderNumber IS NULL
+
