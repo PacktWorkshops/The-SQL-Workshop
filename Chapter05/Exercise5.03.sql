fec385a49477c9a3d310f0cc75ab491c887282a8
ManasaVk243 <manasak@packtpub.com>
2019-12-30 10:22:34 +0530

    Update Exercise5.03.sql

--- Chapter05/Exercise5.03.sql
+++ Chapter05/Exercise5.03.sql
@@ -2,3 +2,8 @@ SELECT ProductName,NetRetailPrice
 FROM Products
 WHERE ProductName != 'tomato sauce'
 ORDER BY NetRetailPrice;
+SELECT ProductName,NetRetailPrice
+FROM Products
+WHERE ProductName <> 'tomato sauce'
+ORDER BY NetRetailPrice;
+
