921064abc1484ae7b2b61a6e016d8a7e77aa59a9
ManasaVk243 <manasak@packtpub.com>
2019-12-30 11:08:40 +0530

    Update Exercise9.02.sql

--- Chapter09/Exercise9.02.sql
+++ Chapter09/Exercise9.02.sql
@@ -1,3 +1,7 @@
 USE packt_online_shop;
 
 GRANT EXECUTE ON PROCEDURE packt_online_shop.spFilterProductsByNRP TO 'TEMP_ACCOUNT_1';
+
+packt_online_shop.spFilterProductsByNRP
+
+SHOW GRANTS FOR TEMP_ACCOUNT_1;
