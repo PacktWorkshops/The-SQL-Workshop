6cb618e56530cf652106a6bab8fe0b771b76ba2b
ManasaVk243 <manasak@packtpub.com>
2020-01-08 15:04:32 +0530

    Update and rename Chapter 1 to Exercise1.01.sql

---
+++ Chapter01/Exercise1.01.sql
@@ -0,0 +1,14 @@
+
+
+create database PACKT_ONLINE_SHOP;
+use PACKT_ONLINE_SHOP;
+create table Customers
+(
+    FirstName varchar(50) ,
+    MiddleName varchar(50) ,
+    LastName varchar(50) ,
+    HomeAddress varchar(250) ,
+    Email varchar(200) ,
+    Phone varchar(50) ,
+    Notes varchar(250)
+);
